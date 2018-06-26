60c3901e382a2dc3f3a92c2db457bb51395e58de
coder-zyq <15521186330@163.com>
2018-06-26 19:21:07 +0800

    router create food,order.

--- conf/config.go
+++ conf/config.go
@@ -1,16 +1,16 @@
 package conf
 
 const (
-  // mysql -u sevenkingdom -h db4free.net -p
-  // USER     string = "cliadmin"
-  // PASSWORD string = "Passw0rd"
+	// mysql -u sevenkingdom -h db4free.net -p
+	// USER     string = "cliadmin"
+	// PASSWORD string = "Passw0rd"
 	// DB       string = "clidb"
 	// HOST     string = "120.25.165.247"
-	PORT     string = "3306"
+	PORT string = "3306"
 
-  // Test: db4free
+	// Test: db4free
 	USER     string = "sevenkingdom"
 	PASSWORD string = "password"
-  DB       string = "cliserver"
-  HOST     string = "db4free.net"
+	DB       string = "cliserver"
+	HOST     string = "db4free.net"
 )
