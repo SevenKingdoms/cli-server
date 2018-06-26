8369ef30c1f23219ab0c2c2bab07403f5c4e4ea6
coder-zyq <15521186330@163.com>
2018-06-26 21:43:24 +0800

    no-test(food.go update)

---
+++ api/food.go
@@ -0,0 +1,89 @@
+package api
+
+import (
+	"strconv"
+	// "fmt"
+	"github.com/Sirupsen/logrus"
+	"github.com/cli-server/model"
+	"github.com/gocraft/dbr"
+	"github.com/labstack/echo"
+	"github.com/valyala/fasthttp"
+)
+
+func PostFood() echo.HandlerFunc {
+	return func(c echo.Context) (err error) {
+		f := new(model.Food)
+		if err = c.Bind(m); err != nil {
+			logrus.Debug(err)
+			return echo.NewHTTPError(fasthttp.StatusInternalServerError)
+		}
+
+		tx := c.Get("Tx").(*dbr.Tx)
+
+		food := model.NewMerchant(f.Id, f.Name, f.Images, f.Type, f.Price,
+			f.HotIndex, f.Introduction, f.Merchant_id)
+
+		if err := food.Save(tx); err != nil {
+			logrus.Debug(err)
+			return echo.NewHTTPError(fasthttp.StatusInternalServerError)
+		}
+		return c.JSON(fasthttp.StatusCreated,
+			NewJSON("OK", "成功创建/更改食品", merchant))
+	}
+}
+
+func GetFoodsByMerchantId() echo.HandlerFunc {
+	return func(c echo.Context) (err error) {
+
+		id := c.QueryParam("merchant_id")
+		number, _ := strconv.ParseInt(id, 0, 64)
+
+		tx := c.Get("Tx").(*dbr.Tx)
+
+		foods := new(model.Foods)
+		if _, err := foods.mearchant_Load(tx, number); err != nil {
+			logrus.Debug(err)
+			return echo.NewHTTPError(fasthttp.StatusNotFound, "Food in this merchant does not exists.")
+		}
+		return c.JSON(fasthttp.StatusOK,
+			NewJSON("OK", "成功获取商家食品", foods))
+
+	}
+}
+func GetFoodByFoodId() echo.HandlerFunc {
+	return func(c echo.Context) (err error) {
+
+		id := c.Param("food_id")
+		number, _ := strconv.ParseInt(id, 0, 64)
+
+		tx := c.Get("Tx").(*dbr.Tx)
+
+		food := new(model.Food)
+		if _, err := food.Load(tx, number); err != nil {
+			logrus.Debug(err)
+			return echo.NewHTTPError(fasthttp.StatusNotFound, "Food  does not exists.")
+		}
+		return c.JSON(fasthttp.StatusOK,
+			NewJSON("OK", "成功获取食品", merchant))
+
+	}
+}
+
+func DeleteFood() echo.HandlerFunc {
+	return func(c echo.Context) (err error) {
+
+		id := c.Param("food_id")
+		number, _ := strconv.ParseInt(id, 0, 64)
+
+		tx := c.Get("Tx").(*dbr.Tx)
+
+		food := new(model.Food)
+		if _, err := food.food_delete(tx, number); err != nil {
+			logrus.Debug(err)
+			return echo.NewHTTPError(fasthttp.StatusNotFound, "Food  does not exists.")
+		}
+		return c.JSON(fasthttp.StatusOK,
+			NewJSON("OK", "成功删除食品", food))
+
+	}
+}
---
+++ model/food.go
@@ -0,0 +1,97 @@
+package model
+
+import (
+	// "time"
+	// "fmt"
+
+	"github.com/gocraft/dbr"
+)
+
+type Food struct {
+	Id           int64   `json:"food_id" form:"food_id" query:"food_id"`
+	Name         string  `json:"food_name" form:"food_name" query:"food_name"`
+	Images       string  `json:"food_images" form:"food_images" query:"food_images"`
+	Type         string  `json:"food_type" form:"food_type" query:"food_type"`
+	Price        float64 `json:"food_price" form:"food_price" query:"food_price"`
+	HotIndex     int64   `json:"hotIndex" form:"hotIndex" query:"hotIndex"`
+	Introduction string  `json:"food_introduction" form:"food_introduction" query:"food_introduction"`
+	Merchant_id  int64   `json:"merchant_id" form:"merchant_id" query:"merchant_id"`
+}
+
+func NewFood(id int64, name, images, food_type string, price float64, hotIndex int64, intro string, merchant_id int64) *Food {
+	return &Food{
+		Id:           id,
+		Name:         name,
+		Images:       images,
+		Type:         food_type,
+		Price:        price,
+		HotIndex:     hotIndex,
+		Introduction: intro,
+		Merchant_id:  merchant_id,
+	}
+}
+
+func (f *Food) Save(tx *dbr.Tx) error {
+
+	var count = 0
+	tempFood := new(Food)
+	count, err := tx.Select("*").
+		From("Food").
+		Where("id = ?", f.Id).
+		Load(&tempFood)
+
+	if count == 0 {
+		// if food not exists, Create
+		_, err = tx.InsertInto("Food").
+			Pair("id", f.Id).
+			Pair("name", f.Name).
+			Pair("images", f.Images).
+			Pair("type", f.Type).
+			Pair("price", f.Price).
+			Pair("hotIndex", f.HotIndex).
+			Pair("introduction", f.Introduction).
+			Pair("Merchant_id", f.Merchant_id).
+			Exec()
+	} else {
+		// if food exists, Update
+		_, err = tx.Update("Food").
+			Set("name", f.Name).
+			Set("images", f.Images).
+			Set("type", f.Type).
+			Set("price", f.Price).
+			Set("hotIndex", f.HotIndex).
+			Set("introduction", f.Introduction).
+			Set("Merchant_id", f.Merchant_id).
+			Where("id = ?", f.id).
+			Exec()
+	}
+
+	return err
+}
+
+func (f *Food) Load(tx *dbr.Tx, id int64) (int, error) {
+	return tx.Select("*").
+		From("Food").
+		Where("id = ?", f.id).
+		Load(f)
+}
+
+func (f *Food) mearchant_Load(tx *dbr, merchant_id int64) (int, error) {
+	return tx.Select("*").
+		From("Food").
+		Where("Merchant_id = ?", f.Merchant_id).
+		Load(f)
+
+}
+
+func (f *Food) food_delete(tx *dbr, food_id int64) (int, error) {
+	return tx.DeleteFrom("Food").Where("id = ?", food_id).Exec()
+}
+
+type Foods []Food
+
+func (u *Foods) Load(tx *dbr.Tx) (int, error) {
+	return tx.Select("*").
+		From("Food").
+		Load(u)
+}
